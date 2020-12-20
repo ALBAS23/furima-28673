a2ae0e114c4c2aaf9d81054f1edbff9dd8a7e72e
ALBAS23 <maxbouxmasa3939@gmail.com>
2020-12-20 14:03:17 +0900

    【Userモデル】バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  columns = [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth]
+
+  with_options presence: true do
+    columns.each do |column|
+      validates column
+  end
+
+  validates :password, format: {with: /\A[a-z\d]{6}+\z/i, message: "半角英数字で入力してください"}
+  
 end
