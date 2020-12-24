3054d6329077b603997c4562c062c76f61bbcb24
ALBAS23 <maxbouxmasa3939@gmail.com>
2020-12-24 16:35:27 +0900

    ActiveHash バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :shipment_prefecture
+  belongs_to :delivery_fee
+  belongs_to :shipment_day
+  belongs_to :status
 end
