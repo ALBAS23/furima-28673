97d5e6863d8fe3698d84d7a9fb755dc8cd0698df
ALBAS23 <maxbouxmasa3939@gmail.com>
2020-12-24 19:09:40 +0900

    アソシエーション設定_2

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,15 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :shipment_day
   belongs_to :status
+
+  belongs_to :user
+
+  columns = [name, explanation, category_id, status_id, delivery_fee_id, shipment_prefecture_id, shipment_day_id, price]
+
+  with_options presence: true do 
+    columns.each do |column|
+      validates column
+    end
+  end
+
 end
