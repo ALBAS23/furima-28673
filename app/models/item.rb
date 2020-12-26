ecaf80e322a577f2f460d89a6ae6b0af729f7341
ALBAS23 <maxbouxmasa3939@gmail.com>
2020-12-26 12:22:40 +0900

    ActiveHash バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,4 +18,12 @@ class Item < ApplicationRecord
     end
   end
 
+  ids = [category_id, status_id, delivery_fee_id, shipment_prefecture_id, shipment_day_id]
+
+  with_options numericality: { other_than: 1 } do
+    ids.each do |id|
+      validates id
+    end
+  end
+
 end
