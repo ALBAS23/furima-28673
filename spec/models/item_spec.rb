32abea64a7857cb4052dc5b88911d43bdabb10df
ALBAS23 <maxbouxmasa3939@gmail.com>
2020-12-27 21:46:00 +0900

    【テストコード】修正#1

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -106,6 +106,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price must be less than or equal to 9999999'
       end
+      # 14
+      it '出品者が紐付かない場合、登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'User must exist'
+      end
     end
   end
 end
