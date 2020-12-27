3219ad02e0880bb29aa3744895c32651ec01b4ad
ALBAS23 <maxbouxmasa3939@gmail.com>
2020-12-27 20:50:42 +0900

    【テストコード】FactoryBot 作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name                        { Faker::Lorem.word }
+    explanation                 { Faker::Lorem.sentence }
+    category_id                 { Faker::Number.between(from: 2, to: 11) }
+    status_id                   { Faker::Number.between(from: 2, to: 3) }
+    delivery_fee_id             { Faker::Number.between(from: 2, to: 7) }
+    shipment_prefecture_id      { Faker::Number.between(from: 2, to: 48) }
+    shipment_day_id             { Faker::Number.between(from: 2, to: 4) }
+    price                       { Faker::Number.between(from: 300, to: 9999999) }
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
+    association :user
   end
 end
