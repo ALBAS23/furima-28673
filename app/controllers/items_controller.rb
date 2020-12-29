2721b931f53b1eb62f01aedbebff3ea5006ceb9c
ALBAS23 <maxbouxmasa3939@gmail.com>
2020-12-29 18:12:51 +0900

    itemsコントローラ編集_#1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :direct,  only: :edit
 
   def index
     @items = Item.order(created_at: :DESC)
@@ -22,10 +23,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :image, :explanation, :category_id, :status_id, :delivery_fee_id,
                                  :shipment_prefecture_id, :shipment_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def direct
+    @item = Item.find(params[:id])
+    unless current_user.nickname == @item.user.nickname
+      redirect_to root_path
+    end
   end
 end
