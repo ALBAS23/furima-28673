4b78e50178b54b513f243bbfdf113440b2630ca1
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-01-05 11:34:55 +0900

    purchase_formモデル 編集#1

---
+++ app/models/purchase_form.rb
@@ -0,0 +1,20 @@
+class PurchaseForm
+
+  include ActiveModel::Model
+
+  attr_accessor :postal_code, :shipment_prefecture_id, :municipality, :address, :building, :phone_number, :user_id, :item_id, :token
+  
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :municipality, :address, :token
+    validates :phone_number, format: { with: /\d{,11}/ }
+  end
+
+  validates :shipment_prefecture_id, numericality:  { other_than: 1 }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    BuyerInformation.create(postal_code: postal_code, shipment_prefecture_id: shipment_prefecture_id, municipality: municipality, address: address, building: building, phone_number: phone_number, order_id: order.id)
+  end
+
+end
